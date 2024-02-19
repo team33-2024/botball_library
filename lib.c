94cda667f26f21b315746a13077ee51c2042847f
team33-2024 <160532583+team33-2024@users.noreply.github.com>
2024-02-19 13:10:32 -0800

    modified line_follow function and added drive function
    for line_follow function:
    - switched up parameter placement by making the direction first parameter instead of the fifth
    - removed "port" parameter and made it a constant instead
    - made it so that distance resets once robot touches line
    
    for drive function:
    - created drive function
    - drive function allows robot to move forward or backward

--- lib.c
+++ lib.c
@@ -10,65 +10,90 @@
 #include <stdbool.h>
 #define left 3
 #define right 0
+#define sensorPort 1
 
-void line_follow(int distance, int speed, int port, float intensity, char *side) {
-  // Parameter 1: distance to travel, in ticks
-  // Parameter 2: speed, in ticks
-  // Parameter 3: port of analog sensor that follows line
+void line_follow(char *side, int distance, int speed, float intensity) {
+  // Parameter 1: direction of line relative to robot (either "r" for right or "l" for left)
+  // Parameter 2: distance to travel, in ticks
+  // Parameter 3: speed, in ticks
   // Parameter 4: intensity of line follow from 0-1 (e.x: 0.5)
-  // Parameter 5: direction of line relative to robot (either "r" for right or "l" for left)
   //////////////////////////////////////////////////////////////////////////////////////////
-  
-  bool touchedLine = false;
-  if (*side == 'r'){
-    cmpc(right);
-    while (abs(gmpc(right)) < distance) {
-      if (analog(port) > 4000) {
-        if (touchedLine == false) {
-          printf("Touched line!\n");
-          touchedLine = true;
-          ao();
-          msleep(500);
-        }else {
-          mav(right,speed);
-          mav(left,speed*(1-intensity));
-        }
+
+ cmpc(right);
+ bool touchedLine = false;
+ if (*side == 'r'){
+   while (abs(gmpc(right)) < distance) {
+     if (analog(sensorPort) > 4000) {
+       if (touchedLine == false) {
+         printf("Touched line!\n");
+         touchedLine = true;
+         ao();
+         cmpc(right);
+         msleep(500);
+       }else {
+         mav(right,speed);
+         mav(left,speed*(1-intensity));
+       }
+     }else {
+       if (touchedLine == false) {
+         mav(right,-1*speed);
+         mav(left,speed);
+       } else {
+         mav(right,speed*(1-intensity));
+         mav(left,speed);
+       }
+     }
+   }
+  } else if (*side == 'l'){
+   while (abs(gmpc(right)) < distance) {
+     if (analog(sensorPort) > 3000) {
+       if (touchedLine == false) {
+         printf("Touched line!\n");
+         touchedLine = true;
+         ao();
+         cmpc(right);
+         msleep(500);
+       }else {
+         mav(right,speed*(1-intensity));
+         mav(left,speed);
+       }
       }else {
         if (touchedLine == false) {
-          mav(right,-1*speed);
-          mav(left,speed);
-        } else {
-          mav(right,speed*(1-intensity));
-          mav(left,speed);
-        }
-      }
+         mav(right,speed);
+         mav(left,-1*speed);
+       } else {
+         mav(right,speed);
+         mav(left,speed*(1-intensity));
+       }
+     }
+   }
+   }else {
+    printf("ERROR: function \"line_follow\" needs its first parameter to either be \"r\" or \"l\"\n");
+  }
+}
+
+void drive(char *direction, int distance, int speed) {
+  // Parameter 1: direction to travel in ("f" for forward, and "b" for backward)
+  // Parameter 2: distance to travel, in ticks
+  // Parameter 3: speed to travel, in ticks
+  /////////////////////////////////////////
+
+  cmpc(right);
+  if (*direction == 'f') {
+    while (abs(gmpc(right)) < distance) {
+      mav(right, speed);
+      mav(left, speed);
     }
+    ao();
+    msleep(250);
+  } else if (*direction == 'b') {
+    while (abs(gmpc(right)) < distance) {
+      mav(right, -1*speed);
+      mav(left, -1*speed);
+    }
+    ao();
+    msleep(250);
   } else {
-    if (*side == 'l'){
-    cmpc(right);
-    while (abs(gmpc(right)) < distance) {
-      if (analog(port) > 3000) {
-        if (touchedLine == false) {
-          printf("Touched line!\n");
-          touchedLine = true;
-          ao();
-          msleep(500);
-        }else {
-          mav(right,speed*(1-intensity));
-          mav(left,speed);
-        }
-      }else {
-        if (touchedLine == false) {
-          mav(right,speed);
-          mav(left,-1*speed);
-        } else {
-          mav(right,speed);
-          mav(left,speed*(1-intensity));
-        }
-      }
-    }
-    }else {
-      printf("ERROR: function \"line_follow\" needs its fifth parameter to either be \"r\" or \"l\"\n");
-    }
-  } 
+    printf("ERROR: function \"drive\" needs its first parameter to either be \"f\" or \"b\"\n");
+  }
 }
